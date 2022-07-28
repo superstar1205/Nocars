b9ea931f5e9303061543ba0f864587564606e78e
prodev-web <devking1205@gmail.com>
2022-07-28 10:30:22 +0900

    Update classes.blade.php

--- resources/views/school/classes.blade.php
+++ resources/views/school/classes.blade.php
@@ -5,6 +5,44 @@
     @include('layouts.flash_message')
 
     <!-- main content -->
+    <nav class="navbar navbar-expand-md cnav-bar navbar-dark fixed-top">
+        <div class="container nav-bcolor">
+            <a class="navbar-brand" href="#"><h3>Logo</h3></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link nav-color nav-setc" href="/classes">Classes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/baseline">Baseline</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/details">School</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/holidays">Holidays</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/report">Report</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/record">Record</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/how">How</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link nav-color" href="/logout">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="main d-flex justify-content-center algin-items-center">
         <div class="container d-flex justify-content-center align-items-center">
             <div class="m-card d-flex justify-content-center align-items-center">
